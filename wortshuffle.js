431085d6c0e746ede155a784ed256eb2183c2c62
Ryoda <73408489+Ryodaa@users.noreply.github.com>
2022-11-02 18:13:21 +0100

    added alot

--- wortshuffle.js
+++ wortshuffle.js
@@ -2,9 +2,11 @@ let woerterEasy = ['Hut', 'Haus', 'Boot', 'Test', 'Buch']; // 1-4 Buchstaben
 let woerterMedium = ['Banane', 'Telefon', 'Ananas', 'Katze', 'Monitor']; // 5-7 Buchstaben
 let woerterHard = ['Computer', 'Fernseher', 'Videospiele', 'Smartphone']; // 8-?? Buchstaben
 
-let randomWort = '';
-let loesungWort = '';
-let shuffldWort = '';
+let randomWort = null;
+let loesungWort = null;
+let shuffldWort = null;
+let difficulty = null;
+let uppercaseTick = null;
 let leben = 0;
 
 let prompt = require('prompt-sync')({
@@ -12,36 +14,49 @@ let prompt = require('prompt-sync')({
 });
 
 console.log(`
-Schwierigkeitsgrad wählen:
-1: Easy -- 2: Medium -- 3: Hard
-`)
-let diffInput = prompt('Eingabe: ')
+        Schwierigkeitsgrad wählen:
+        1: Easy -- 2: Medium -- 3: Hard`);
+let diffInput = Number(prompt('Eingabe: '));
+
 
 switch (diffInput) {
     case 1:
-        randomWort += woerterEasy[Math.floor(Math.random() * woerterEasy.length)];
-        loesungWort += randomWort;
-        shuffldWort += shuffleWort(randomWort);
-        leben += 10;
+        randomWort = woerterEasy[Math.floor(Math.random() * woerterEasy.length)];
+        loesungWort = randomWort;
+        shuffldWort = shuffleWort(randomWort);
+        leben = 10;
+        difficulty = 'Easy';
         break;
     case 2:
-        randomWort += woerterMedium[Math.floor(Math.random() * woerterMedium.length)];
-        loesungWort += randomWort;
-        shuffldWort += shuffleWort(randomWort);
-        leben += 7;
+        randomWort = woerterMedium[Math.floor(Math.random() * woerterMedium.length)];
+        loesungWort = randomWort;
+        shuffldWort = shuffleWort(randomWort);
+        leben = 7;
+        difficulty = 'Medium';
         break;
     case 3:
-        randomWort += woerterHard[Math.floor(Math.random() * woerterHard.length)];
-        loesungWort += randomWort;
-        shuffldWort += shuffleWort(randomWort);
-        leben += 4;
+        randomWort = woerterHard[Math.floor(Math.random() * woerterHard.length)];
+        loesungWort = randomWort;
+        shuffldWort = shuffleWort(randomWort);
+        leben = 4;
+        difficulty = 'Hard';
         break;
     default:
         break;
 }
+console.log(`
+        Uppercase Zeichen anzeigen?:
+        1: Yes -- 2: No`);
+let inputUpper = Number(prompt('Eingabe: '));
+
+if (inputUpper === 1) {
+    uppercaseTick = true;
+} else {
+    uppercaseTick = false;
+}
 
 function shuffleWort(wort) {
-    let shuffWort = ''
+    let shuffWort = '';
     let oldWort = wort;
     let newWort = wort;
     for (i in wort) {
@@ -56,23 +71,36 @@ function shuffleWort(wort) {
     }
 };
 
+console.log(`
+        Schwierigkeitsgrad ist: ${difficulty}
+        Du hast ${leben} leben`);
 
-let mischWort = shuffleWort(randomWort);
 while (true) {
-    console.log(`Das gemischte Wort ist: ${mischWort}`)
+    if (uppercaseTick === true) {
+        console.log(`
+        Das gemischte Wort ist: ${shuffldWort}`);
+    } else {
+        console.log(`
+        Das gemischte Wort ist: ${shuffldWort.toLowerCase()}`);
+    }
 
-    let input = prompt('Eingabe: ');
+    let input = prompt('Eingabe: ').toLowerCase();
 
-    if (input == loesungWort) {
-        console.log(`Richtig! ${loesungWort} war die Antwort.`);
+    if (input == loesungWort.toLowerCase()) {
+        console.log(`
+        Richtig! ${loesungWort} war die Antwort.`);
         break;
     } else {
         leben--;
-        console.log(`Leider Falsch. ${leben} leben verbleiben.`)
+        console.log(`
+        Leider Falsch. ${leben} leben verbleiben.`);
         if (leben <= 0) {
-            console.log('Game over')
+            console.log(`
+        Game over`);
             break;
         }
     }
 
 }
+console.log(`
+        Danke fürs Spielen!`)
